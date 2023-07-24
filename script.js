23520a6a8f6c4552bdd5df89dc13cb09b0df824e
Aksh Walia <akshwalia20@gmail.com>
2023-07-24 15:46:33 +0530

    Complete the on-screen calculator
    Completed the on-screen calculator, with every feature except the decimal, going to add that now. Also have to add the keyboard support.

---
+++ script.js
@@ -0,0 +1,119 @@
+const numbers = document.querySelectorAll(".numbers");
+const operators = document.querySelectorAll(".operators");
+const clear = document.getElementById("clear");
+const equals = document.getElementById("equals");
+
+const topDisplay = document.getElementById('top');
+const bottomDisplay = document.getElementById('bottom');
+const del = document.getElementById('delete');
+
+let firstOperand="", secondOperand="", operator="";
+let firstMinus="";
+
+
+
+function updateFirstOperand(e) {
+    firstOperand+=`${firstMinus}${e.target.id}`;
+    firstMinus="";
+    bottomDisplay.textContent=firstOperand;
+}
+
+function updateSecondOperand(e) {
+    secondOperand+=`${e.target.id}`;
+    topDisplay.textContent=firstOperand+operator;
+    bottomDisplay.textContent=secondOperand;
+}
+
+function updateOperator(e) {
+    operator=`${e.target.id}`;
+    topDisplay.textContent=firstOperand;
+    bottomDisplay.textContent=operator;
+}
+
+function result() {
+    if(operator=='+')
+        return Number(firstOperand)+Number(secondOperand);
+    else if(operator=='-')
+        return Number(firstOperand)-Number(secondOperand);
+    else if(operator=='x')
+        return Number(firstOperand)*Number(secondOperand);
+    else if(operator=='/')
+        return Number(firstOperand)/Number(secondOperand);
+    else if(operator=='%')
+        return Number(firstOperand)%Number(secondOperand);
+}
+
+numbers.forEach(num => {
+    num.addEventListener('click', (e) => {
+        if(secondOperand==="flag") {
+            firstOperand="";
+            secondOperand="";
+            operator="";
+        }
+        if(operator==="") {
+            updateFirstOperand(e);
+            console.log(firstOperand);
+        }
+        else {
+            updateSecondOperand(e);
+            console.log(secondOperand);
+        }
+    })
+});
+
+operators.forEach(op => {
+    op.addEventListener('click', (e) => {
+        if(firstOperand=="" && e.target.id!=='-')
+        ;
+        else if(firstOperand=="" && e.target.id=='-'){
+            firstMinus='-';
+            bottomDisplay.textContent='-';
+        }
+        else if(secondOperand!=="" && secondOperand!=="flag"){
+            let res = result();
+            if(!Number.isInteger(res))
+                res=res.toFixed(2);
+            firstOperand=res;
+            secondOperand="";
+            updateOperator(e);
+        }
+        else if(secondOperand=="" || secondOperand==="flag")
+            updateOperator(e);
+            secondOperand="";
+    })
+});
+
+equals.addEventListener('click', () => {
+    let ans=result();
+    topDisplay.textContent="";
+    if(!Number.isInteger(ans))
+        ans=ans.toFixed(2);
+    firstOperand=ans;
+    operator="";
+    secondOperand="flag";
+    bottomDisplay.textContent=ans;
+});
+
+del.addEventListener('click', () => {
+    if(secondOperand!=="") {
+        secondOperand=secondOperand.substring(0,secondOperand.length-1);
+        bottomDisplay.textContent=secondOperand;
+    }
+    else if (operator!=="") {
+        operator="";
+        bottomDisplay.textContent=operator;
+    }
+    else if(firstOperand!=="") {
+        firstOperand=firstOperand.substring(0,firstOperand.length-1);
+        bottomDisplay.textContent=firstOperand;
+    }
+});
+
+clear.addEventListener('click', () => {
+    bottomDisplay.textContent="";
+    topDisplay.textContent="";
+    firstOperand="";
+    secondOperand="";
+    operator="";
+});
+
